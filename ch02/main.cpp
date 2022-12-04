103380353cf9d6ce38b271b598742a78c7c323f7
TaoistQu <13110402600@163.com>
2022-12-04 15:37:46 +0800

    上传main函数

---
+++ ch02/main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include "include/teacher.h"
+using namespace std;
+
+#define Max(a,b) (a)>(b) ? (a):(b)
+#define ADD(x,y) x+y
+
+inline int test(int a,int b){
+    return a+b;
+}
+
+inline int testInline(int x,int y){
+    return x+y;
+}
+
+void test01(){
+    int a =10;
+    int &b = a;
+    cout <<"a:" << a << endl;
+    cout << "b:" << b << endl;
+    cout << "------------" << endl;
+
+    b =100;
+    cout <<"a:" << a << endl;
+    cout << "b:" << b << endl;
+    cout << "------------" << endl;
+
+    int &c = a;
+    c = 200;
+    cout <<"a:" << a << endl;
+    cout << "b:" << b << endl;
+    cout<<"c:" << c << endl;
+    cout << "------------" << endl;
+    cout <<"&a :" << &a<<endl;
+    cout << "&b :" << &b<<endl;
+    cout << "&c :" << &c <<endl;
+}
+
+int main() {
+    //test01();
+   //引用所占的内存空间大小
+   float ch ='a';
+   float & p =ch;
+   cout << sizeof(p) << endl;
+   Teacher teacher;
+   teacher.age =20;
+   printfT2(teacher);
+   printfT1(&teacher);
+   cout << teacher.age << endl;
+    int ret = Max(10,20);
+    cout << ret << endl;
+    cout << ADD(12,23) *10 << endl; // cout << 12 +23*10 << endl
+    cout << test(12,34) *10 << endl;
+    cout << testInline(10,20) << endl;
+   return 0;
+}
